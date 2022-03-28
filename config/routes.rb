b16373745a64ef6c0c1402f48e504c1b9343677a
Timo <timowester@gmail.com>
2022-03-28 23:15:14 +0200

    Set up controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  get 'groups/index'
+  get 'groups/show'
+  get 'groups/new'
+  get 'groups/create'
+  get 'groups/destroy'
+  get 'index/show'
+  get 'index/new'
+  get 'index/create'
+  get 'index/destroy'
+  get 'spendings/new'
+  get 'spendings/create'
+  get 'spendings/destroy'
+  get 'users/home'
   devise_for :users
   # get 'home/index'
   root 'home#index'
