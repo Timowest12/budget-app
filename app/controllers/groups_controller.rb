8701fb6113cb8a5d6b36d36bd47a373fbe74e646
Timo <timowester@gmail.com>
2022-03-29 01:01:03 +0200

    Groups show page

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,16 +5,29 @@ class GroupsController < ApplicationController
   end
 
   def show
-    @groups = Group.find(params[:id])
-    @group_spendings = @groups.spendings.all
+    @group = Group.find(params[:id])
+    @group_spendings = @group.spendings.all
   end
 
   def new
+    @group = Group.new
   end
 
   def create
+    @group = current_user.groups.create(group_params)
+    if @group.save
+      flash[:notice] = 'Category Created!'
+      redirect_to root_path
+    else
+      render :new
+
+    end
   end
 
   def destroy
+  end
+
+  def group_params
+    params.require(:group).permit(:Name, :Icon)
   end
 end
