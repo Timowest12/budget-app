baffd08fb0b8f526e892fcf7015f97558cf316cc
Timo <timowester@gmail.com>
2022-03-28 21:02:58 +0200

    Add entity model

---
+++ db/migrate/20220328190150_create_entities.rb
@@ -0,0 +1,11 @@
+class CreateEntities < ActiveRecord::Migration[7.0]
+  def change
+    create_table :entities do |t|
+      t.integer :AuthorId
+      t.string :Name
+      t.decimal :Amount
+
+      t.timestamps
+    end
+  end
+end
