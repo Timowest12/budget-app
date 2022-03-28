6af5a96dd678d4ab34c4f008cfbc7f07a27aa654
Timo <timowester@gmail.com>
2022-03-29 00:39:40 +0200

    Add and configure groups cntrlr and views

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,8 +1,12 @@
 class GroupsController < ApplicationController
+  
   def index
+    @groups = Group.where(user_id: current_user.id)
   end
 
   def show
+    @groups = Group.find(params[:id])
+    @group_spendings = @groups.spendings.all
   end
 
   def new
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
   def home
+    render :layout => false 
   end
 end
